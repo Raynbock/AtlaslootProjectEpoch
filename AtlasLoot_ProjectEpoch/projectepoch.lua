01b5336e395f24fa1149f109b4f5aa153b76d3d1
Raynbock <116601975+Raynbock@users.noreply.github.com>
2023-04-30 22:33:04 +0200

    Update projectepoch.lua

--- AtlasLoot_ProjectEpoch/projectepoch.lua
+++ AtlasLoot_ProjectEpoch/projectepoch.lua
@@ -723,17 +723,17 @@ local BabbleFaction = AtlasLoot_GetLocaleLibBabble("LibBabble-Faction-3.0")
 
 	AtlasLoot_Data["Glittermurk"] = {
 		{ 1, 0, "INV_Box_01", "=q6=Supervisor Grimgash", ""};
-		{ 1, 0, "INV_Box_01", "=q6=Foreman Sprocket", ""};
-		{ 2, 60986, "", "=q3=Heavy Metal Belt", "", "", ""};
-		{ 3, 60991, "", "=q3=Shredder Spaulders", "", "", ""};
-		{ 1, 0, "INV_Box_01", "=q6=???", ""};
-		{ 1, 0, "INV_Box_01", "=q6=Prismscale", ""};
-		{ 5, 0, "INV_Box_01", "=q6=Murklurk", ""};
-		{ 6, 61058, "", "=q3=Shadowcasters Hood", "", "", ""};
-		{ 7, 60970, "", "=q3=Amulet of Brawn", "", "", ""};
-		{ 9, 0, "INV_Box_01", "=q6=Gnash", ""};
-		{ 10, 60987, "", "=q3=Naga Precision", "", "", ""};
-		{ 11, 60982, "", "=q3=Encrusted Fetish", "", "", ""};
+		{ 3, 0, "INV_Box_01", "=q6=Foreman Sprocket", ""};
+		{ 4, 60986, "", "=q3=Heavy Metal Belt", "", "", ""};
+		{ 5, 60991, "", "=q3=Shredder Spaulders", "", "", ""};
+		{ 7, 0, "INV_Box_01", "=q6=???", ""};
+		{ 9, 0, "INV_Box_01", "=q6=Prismscale", ""};
+		{ 11, 0, "INV_Box_01", "=q6=Murklurk", ""};
+		{ 12, 61058, "", "=q3=Shadowcasters Hood", "", "", ""};
+		{ 13, 60970, "", "=q3=Amulet of Brawn", "", "", ""};
+		{ 16, 0, "INV_Box_01", "=q6=Gnash", ""};
+		{ 17, 60987, "", "=q3=Naga Precision", "", "", ""};
+		{ 18, 60982, "", "=q3=Encrusted Fetish", "", "", ""};
 		Back = "DUNGEONMENU";
 	};
 
