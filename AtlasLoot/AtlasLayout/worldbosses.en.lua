fb91b315b3a85dc7b19f6a7749e4402b0d8700f8
Raynbock <116601975+Raynbock@users.noreply.github.com>
2023-11-16 03:33:26 +0100

    Add files via upload

---
+++ AtlasLoot/AtlasLayout/worldbosses.en.lua
@@ -0,0 +1,31 @@
+--[[
+worldbosses.en.lua
+This file defines the loot buttons in the Atlas interface for the WorldBosses maps.
+]]
+
+AtlasLootWBBossButtons = {
+
+    FourDragons = {
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "";
+        "DLethon";
+        "DEmeriss";
+        "DTaerar";
+        "DYsondre";
+        };
+
+    Azuregos = {
+        "WorldBossesClassic";
+        };
+
+    };
